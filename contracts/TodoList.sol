f7afe0863d20b12eb65e83f0133bbe33b634f4ea
jayson-temporas <jayson.temporas@gmail.com>
2021-08-16 10:17:39 +0800

    init

---
+++ contracts/TodoList.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+    bool deleted;
+  }
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed,
+    bool deleted
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  event TaskDeleted(
+    uint id,
+    bool deleted
+  );
+
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    createTask("Buy food");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false, false);
+    emit TaskCreated(taskCount, _content, false, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
+  function deleteTask(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.deleted = true;
+    tasks[_id] = _task;
+    emit TaskDeleted(_id, _task.deleted);
+  }
+
+}
